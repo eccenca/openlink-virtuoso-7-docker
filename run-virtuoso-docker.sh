649a647a708ce5a4dfc4774061e64cfdb862cb39
Armando Barbosa <armandobs14@gmail.com>
2016-02-05 16:25:56 -0300

    Adding gitignore

--- run-virtuoso-docker.sh
+++ run-virtuoso-docker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --name $1 -p 8890:8890 -p 1111:1111 -d eccenca/virtuoso7
+#docker run --name $1 -p 8890:8890 -p 1111:1111 -d eccenca/virtuoso7
+docker run --name $1 -p 8890:8890 -p 1111:1111 -v virtuoso.db:/var/lib/virtuoso/db/virtuoso.db -d eccenca/virtuoso7
