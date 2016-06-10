59e150787d89eef17e5ee7b9531a93d687242c36
Henri Knochenhauer <henri.knochenhauer@eccenca.com>
2016-06-10 16:31:12 +0200

    update virtuosohelper

--- assets/virtuoso_helper.sh
+++ assets/virtuoso_helper.sh
@@ -24,6 +24,21 @@ importData () {
 
 }
 
+deleteData(){
+  if [ -z ${oldpassword} ]; then
+    echo "Please enter the current password (default pw is dba)"
+    return 1
+  fi
+
+  if [ -z ${graph} ]; then
+    echo "Please specify a graph which should be deleted"
+    return 1
+  fi
+
+  /opt/virtuoso-opensource/bin/isql 1111 dba $oldpassword exec="SPARQL DROP SILENT GRAPH <${graph}>;"
+
+}
+
 backupData () {
 
   if [ -z ${oldpassword} ]; then
@@ -54,7 +69,7 @@ restoreData () {
   BACKUPDIR="$VIRT_DB/backup"
   cd $BACKUPDIR
   /opt/virtuoso-opensource/bin/virtuoso-t -c $CONFIG +foreground +restore-backup $backupprefix
-  
+
 }
 
 changeAdminPassword () {
@@ -76,6 +91,7 @@ changeAdminPassword () {
 appHelp () {
   echo "Available options:"
   echo " app:importData [DBA-PASSWD, FILENAME, GRAPH]               - import given FILENAME to requested GRAPH "
+  echo " app:deleteData [DBA-PASSWD, GRAPH]                         - delete requested GRAPH"
   echo " app:backupData [DBA-PASSWD]                                - create a backup with todays timestamp in $VIRT_DB/backup"
   echo " app:restoreData [BACKUP-PREFIX]                            - restore a backup with given backup-prefix (e.g. virt_backup_yymmdd-hhmm#)"
   echo " app:changeAdminPassword [OLD-DBA-PASSWD, NEW-DBA-PASSWD]   - change the admin password"
@@ -90,7 +106,13 @@ case "$1" in
     filename=$2
     graph=$3
     importData
-    ;; 
+    ;;
+  app:deleteData)
+    shift
+    oldpassword=$1
+    graph=$2
+    deleteData
+    ;;
   app:backupData)
     shift
     oldpassword=$1
@@ -100,13 +122,13 @@ case "$1" in
     shift
     backupprefix=$1
     restoreData
-    ;;     
+    ;;
   app:changeAdminPassword)
     shift
     oldPW=$1
     newPW=$2
     changeAdminPassword
-    ;;   
+    ;;
   app:help)
     appHelp
     ;;
